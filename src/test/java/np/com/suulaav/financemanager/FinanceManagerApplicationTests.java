c2f914dee4129d91fd6e3831850ec460ed2aaecf
Sulav <sulav@aricalot.com>
2022-12-02 22:38:45 +0545

    initial

---
+++ src/test/java/np/com/suulaav/financemanager/FinanceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package np.com.suulaav.financemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
