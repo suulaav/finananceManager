4b5ce9afc966eedc9bebc965126742cef32c786e
Sulav <sulav@aricalot.com>
2022-12-03 22:52:00 +0545

    User CRUD

--- src/main/java/np/com/suulaav/financemanager/controller/user/UserController.java
+++ src/main/java/np/com/suulaav/financemanager/controller/user/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,7 +36,7 @@ public class UserController {
     return userCrudService.insert(userDomain);
   }
 
-  @GetMapping
+  @PutMapping
   public String update(UserDomain userDomain) {
     log.debug("Inside : User -> Update");
     return userCrudService.update(userDomain);
